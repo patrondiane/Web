d2b0aea19826450091bee67a67cfc1f57518615e
patrondiane <61540042+patrondiane@users.noreply.github.com>
2020-04-30 20:46:03 +0200

    Create ParticipantsList.vue

---
+++ src/ParticipantsList.vue
@@ -0,0 +1,13 @@
+<template>
+  <ol>
+    <li v-for="person in list" :key="person">
+      {{ person.firstname }} {{ person.lastname }}
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: ['list'],
+};
+</script>
