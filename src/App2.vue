e90ceb7aaedf3d1da754749a75138d570ee92097
patrondiane <61540042+patrondiane@users.noreply.github.com>
2020-04-30 20:38:54 +0200

    Create App2.vue

---
+++ src/App2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <h1>Participants list</h1>
+      <participants-list :list="participants"></participants-list>
+    </div>
+
+    <h3>New participant</h3>
+    <new-participant-form @added="addNewParticipant($event)"></new-participant-form>
+  </div>
+</template>
+
+<script>
+  import NewParticipantForm from "./NewParticipantForm.vue";
+  import ParticipantsList from "./ParticipantsList.vue";
+
+  export default {
+    components: {NewParticipantForm, ParticipantsList},
+    data() {
+      return {
+        participants: []
+      };
+    },
+    methods: {
+      addNewParticipant(newParticipant) {
+        this.participants.push(newParticipant);
+      }
+    }
+  };
+</script>
